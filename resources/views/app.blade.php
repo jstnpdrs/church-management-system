49682a02e5a525ff28d4bea77f060e7feffe7ece
jstnpdrs <pedrosaj24@example.com>
2022-10-26 22:15:00 +0800

    add icon to title bar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<link rel = "icon" href =
+"/ACC-LOGO.png"
+        type = "image/x-icon">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
